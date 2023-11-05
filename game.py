b337edd9f35a5e37f88c42ce8da00c0394a32d77
SashaB456 <bilsss032518@gmail.com>
2023-11-05 14:42:30 +0200

    Added score count

--- game.py
+++ game.py
@@ -4,8 +4,10 @@ WIDTH = 1200
 HEIGHT = 600
 SIZE = (WIDTH, HEIGHT)
 window = pygame.display.set_mode(SIZE)
-bg_col = (0, 255, 0)
-window.fill(bg_col)
+bg = pygame.transform.scale(pygame.image.load('background.png'), (WIDTH, HEIGHT))
+score = 0
+pygame.font.init()
+font2 = pygame.font.Font(None, 40)
 clock = pygame.time.Clock()
 class GameSprite(pygame.sprite.Sprite):
     def __init__(self, filename, coords, size, speed):
@@ -26,18 +28,24 @@ class Tube(GameSprite):
     def update1(self):
         self.rect.x -= self.speed
         if self.rect.x <= 0:
-            self.rect.x = WIDTH
+            self.rect.x = WIDTH + randint(0, 100)
             self.rect = pygame.Rect(self.rect.x,self.rect.y, self.rect.width, randint(50, HEIGHT/2-50))
             self.image = pygame.transform.scale(self.original, (self.rect.width,self.rect.height))
+            global score
+            score += 1
+            self.speed = randint(3, 10)
     def update2(self):
         self.rect.x -= self.speed
         if self.rect.x <= 0:
-            self.rect.x = WIDTH
+            self.rect.x = WIDTH + randint(0, 100)
             self.rect = pygame.Rect(self.rect.x,randint(HEIGHT/2, HEIGHT-100), self.rect.width, self.rect.height)
             self.image = pygame.transform.scale(self.original, (self.rect.width,self.rect.height))
+            global score
+            score += 1
+            self.speed = randint(3, 10)
 bird = Bird('bird.png', (50, HEIGHT/2), (50, 50), 5)
-tube1 = Tube('tube1.png', (WIDTH+randint(0, 100), 100), (75, 200), 4)
-tube2 = Tube('tube2.png', (WIDTH-randint(0, 100), HEIGHT+100), (75, 600), 4)
+tube1 = Tube('tube_up.png', (WIDTH, 100), (75, 200), 4)
+tube2 = Tube('tube_down.png', (WIDTH, HEIGHT+100), (75, 600), 4)
 game = True
 finish = False
 while game == True:
@@ -45,7 +53,7 @@ while game == True:
         if event.type == pygame.QUIT:
             game = False
     if not finish:
-        window.fill(bg_col)
+        window.blit(bg, (0, 0))
         bird.reset()
         bird.update()
         tube1.update1()
@@ -58,5 +66,7 @@ while game == True:
             font1 = pygame.font.Font(None, 60)
             text1 = font1.render('Ти програв!', True, (255, 0, 0))
             window.blit(text1, (WIDTH/2, HEIGHT/2))
+        score_text = font2.render('Очки:' + str(score), True, (255, 255, 255))
+        window.blit(score_text, (0, 0))
     pygame.display.update()
     clock.tick(60)
